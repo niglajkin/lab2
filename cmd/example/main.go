8a79d9b9bc5bd64fe441f0772b107184153f650e
Maks-Ytka <maksym.kozlovskiy@gmail.com>
2025-05-29 19:48:23 +0300

    Add buildHandler() func to main.go

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,26 +3,49 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"os"
+	"strings"
 
 	lab2 "github.com/niglajkin/lab2"
 )
 
+// command-line flags (defined outside main, as in the samples)
 var (
-	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	exprFlag = flag.String("e", "", "postfix expression (mutually exclusive with -f)")
+	fileFlag = flag.String("f", "", "path to file containing postfix expression (mutually exclusive with -e)")
+	outFlag  = flag.String("o", "", "output file (optional, stdout by default)")
 )
 
-func main() {
-	flag.Parse()
+// buildHandler wires IO according to parsed flags and returns a ready ComputeHandler
+func buildHandler() (*lab2.ComputeHandler, error) {
+	bothProvided := *exprFlag != "" && *fileFlag != ""
+	noneProvided := *exprFlag == "" && *fileFlag == ""
+	if bothProvided || noneProvided {
+		return nil, fmt.Errorf("specify exactly one of -e or -f")
+	}
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	var in io.Reader
+	if *exprFlag != "" {
+		in = strings.NewReader(*exprFlag)
+	} else {
+		f, err := os.Open(*fileFlag)
+		if err != nil {
+			return nil, fmt.Errorf("opening input file: %w", err)
+		}
+		in = f
+	}
 
-	res, _ := lab2.PostfixToInfix("+ 2 2")
-	fmt.Println(res)
+	var out io.Writer
+	if *outFlag != "" {
+		f, err := os.Create(*outFlag)
+		if err != nil {
+			return nil, fmt.Errorf("creating output file: %w", err)
+		}
+		out = f
+	} else {
+		out = os.Stdout
+	}
+
+	return &lab2.ComputeHandler{Input: in, Output: out}, nil
 }
