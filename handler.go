d844c6c9c0967a4e6c389e72513e4bc7f4607d5d
Ihlaikin Mykola <Ggg501434@gmail.com>
2025-05-29 19:37:21 +0300

    Add toUTF8 func in handler.go for correc file's handling

--- handler.go
+++ handler.go
@@ -1,9 +1,35 @@
 package lab2
 
+import (
+	"encoding/binary"
+	"unicode/utf16"
+)
+
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
 	// TODO: Add necessary fields.
+}
+
+func toUTF8(raw []byte) string {
+	if len(raw) >= 2 {
+		if raw[0] == 0xFF && raw[1] == 0xFE {
+			u16 := make([]uint16, 0, (len(raw)-2)/2)
+			for i := 2; i+1 < len(raw); i += 2 {
+				u16 = append(u16, binary.LittleEndian.Uint16(raw[i:]))
+			}
+			return string(utf16.Decode(u16))
+		}
+
+		if raw[0] == 0xFE && raw[1] == 0xFF {
+			u16 := make([]uint16, 0, (len(raw)-2)/2)
+			for i := 2; i+1 < len(raw); i += 2 {
+				u16 = append(u16, binary.BigEndian.Uint16(raw[i:]))
+			}
+			return string(utf16.Decode(u16))
+		}
+	}
+	return string(raw)
 }
 
 func (ch *ComputeHandler) Compute() error {
