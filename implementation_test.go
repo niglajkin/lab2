cf4f98c735246d652ad70ae7dc0de58a88610be5
Maks-Ytka <maksym.kozlovskiy@gmail.com>
2025-05-29 18:43:26 +0300

    Add tests for PrefixToInfix func

--- implementation_test.go
+++ implementation_test.go
@@ -8,16 +8,52 @@ import (
 )
 
 func TestPostfixToInfix(t *testing.T) {
-	res, err := PostfixToInfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+	valid := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{"add", "2 3 +", "2 + 3"},
+		{"sub", "4 2 -", "4 - 2"},
+		{"mul", "6 7 *", "6 * 7"},
+		{"div", "8 2 /", "8 / 2"},
+		{"pow", "3 2 ^", "3 ^ 2"},
+
+		{"reorder", "4 2 - 3 * 5 +", "5 + 3 * (4 - 2)"},
+
+		{"complex7", "1 2 + 3 * 4 5 - / 6 7 ^ +", "3 * (1 + 2) / (4 - 5) + 6 ^ 7"},
+		{"complex10", "1 2 + 3 * 4 5 - 6 / ^ 7 8 + * 9 / 10 -", "((3 * (1 + 2)) ^ ((4 - 5) / 6)) * (7 + 8) / 9 - 10"},
+	}
+
+	for _, tc := range valid {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := PostfixToInfix(tc.input)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestPostfixToInfix_Errors(t *testing.T) {
+	bad := []string{
+		"",
+		"5 +",
+		"2 3 4",
+		"2 3 + +",
+		"2 3 &",
+	}
+
+	for _, input := range bad {
+		t.Run(fmt.Sprintf("error_%q", input), func(t *testing.T) {
+			_, err := PostfixToInfix(input)
+			assert.Error(t, err)
+		})
 	}
 }
 
 func ExamplePostfixToInfix() {
-	res, _ := PostfixToInfix("+ 2 2")
+	res, _ := PostfixToInfix("1 2 + 3 * 4 5 - / 6 7 ^ +")
 	fmt.Println(res)
-
 	// Output:
-	// 2 2 +
+	// 3 * (1 + 2) / (4 - 5) + 6 ^ 7
 }
