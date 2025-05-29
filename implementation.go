ec1c1bfdb64c38ad0e306ee6369f29cc5989c46a
Ihlaikin Mykola <Ggg501434@gmail.com>
2025-05-29 16:32:17 +0300

    Rename func in implementation.go to match lab variant

--- implementation.go
+++ implementation.go
@@ -3,7 +3,7 @@ package lab2
 import "fmt"
 
 // TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
+// PostfixToInfix converts
+func PostfixToInfix(input string) (string, error) {
 	return "TODO", fmt.Errorf("TODO")
 }
